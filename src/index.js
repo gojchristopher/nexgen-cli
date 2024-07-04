fcac68458f4ed276d9450aa3931caebca88aa661
John Christopher Go <go.jchristopher@gmail.com>
2024-07-04 23:30:03 +0800

    chore: moved to index.js

---
+++ src/index.js
@@ -0,0 +1,24 @@
+#!/usr/bin/env node
+
+import chalk from "chalk";
+import { program } from "commander";
+import figlet from "figlet";
+import inquirer from "inquirer";
+import { generateBrand } from "./generateBrand.js";
+import { questions } from "./utils/questions.js";
+
+program.version("1.0.0").description("Generate new brand");
+
+console.log(
+  chalk.yellow(
+    figlet.textSync("Generate New Brand", { horizontalLayout: "full" })
+  )
+);
+
+program.action(() => {
+  inquirer.prompt(questions).then(async (result) => {
+    await generateBrand(result);
+  });
+});
+
+program.parse(process.argv);
